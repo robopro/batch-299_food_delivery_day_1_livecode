26e8f60acf77c507e64746879dda02fdad8e7881
Morten Salvesen <msalvefirm@gmail.com>
2019-08-27 18:55:41 +0200

    First commit: Code cleaned.

---
+++ test.rb
@@ -0,0 +1,19 @@
+require_relative "app/models/meal"
+require_relative "app/repositories/meal_repository"
+
+require_relative "app/models/customer"
+require_relative "app/repositories/customer_repository"
+
+# *** WRITE YOUR OWN CODE FOR TESTING IN HERE ***
+
+
+# EXAMPLE TESTS
+# Meal class doesn't care about spaceships =(
+# meal = Meal.new(name: "pizza", spaceship: "Apollo")
+# p meal
+
+# repo = MealRepository.new('data/meals.csv')
+# p repo
+
+# customer_repo = CustomerRepository.new('data/customers.csv')
+# p customer_repo
